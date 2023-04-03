c5bb741eaa7e9b05b3732ae6e31aa47dc098ad86
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2023-04-04 00:36:23 +0530

    Update and rename web-script-14-01.ps1 to web-script-13-01.ps1

--- web-script-13-01.ps1
+++ web-script-13-01.ps1
@@ -5,8 +5,8 @@ install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
 install-windowsfeature web-mgmt-tools
 Set-Location -Path C:\inetpub\wwwroot
 $shell_app = new-object -com shell.application
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/ahmadzahoory/gcpcev2/raw/main/lab-14-01-code.zip", (Get-Location).Path + "\lab-14-01-code.zip")
-$zipfile = $shell_app.Namespace((Get-Location).Path + "\lab-14-01-code.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/ahmadzahoory/gcpcev2/raw/main/lab-13-01-code.zip", (Get-Location).Path + "\lab-13-01-code.zip")
+$zipfile = $shell_app.Namespace((Get-Location).Path + "\lab-13-01-code.zip")
 $destination = $shell_app.Namespace((Get-Location).Path)
 $destination.copyHere($zipfile.items())
 New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\vnet-app-website -Site "Default Web Site" -force
