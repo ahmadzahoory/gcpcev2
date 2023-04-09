02fd08275fb0e299a4de432bc7ca978305a4a97d
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2023-04-09 14:28:18 +0530

    Create web-script-08-01.sh

---
+++ web-script-08-01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get -y update
+apt-get install -y apache2
+apt-get install -y unzip
+systemctl restart apache2.service
+cd /var/www/html/ 
+rm index.html
+wget https://github.com/ahmadzahoory/gcpcev2/raw/main/lab-08-01-code.zip
+unzip lab-08-01-code.zip
+useradd -m -s /bin/bash gcpadmin
+usermod -aG sudo gcpadmin
+echo "gcpadmin:lab-password" | chpasswd
+sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
+systemctl restart sshd.service
