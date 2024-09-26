438743ef5a3b63c79952a94ad5be2df42b56c122
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2024-09-26 17:35:09 +0530

    Update script-05-02.sh

--- script-05-02.sh
+++ script-05-02.sh
@@ -1,5 +1,5 @@
-useradd -m -s /bin/bash gcpadmin
 usermod -aG sudo gcpadmin
 echo "gcpadmin:lab-password" | chpasswd
-sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
-systemctl restart sshd.service
+sed -i 's|[#]*KbdInteractiveAuthentication no|#KbdInteractiveAuthentication no|g' /etc/ssh/sshd_config
+sed -i 's|[#]*#PasswordAuthentication yes|PasswordAuthentication yes|g' /etc/ssh/sshd_config
+systemctl restart ssh
