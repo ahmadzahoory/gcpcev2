cec178f6a70784741df07479b5ffdd4f6363ae7e
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2024-10-09 22:45:10 +0530

    Update web-script-08-01.sh

--- web-script-08-01.sh
+++ web-script-08-01.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-apt-get -y update
-apt-get install -y apache2
-apt-get install -y unzip
+# Deploy Web server
+sudo apt-get -y update
+sudo apt-get install -y apache2
+sudo apt-get install -y unzip
 apt-get install -y stress-ng
-apt-get install -y php7.4
-systemctl restart apache2.service
-cd /var/www/html/ 
-rm index.html
-wget https://github.com/ahmadzahoory/gcpcev2/raw/main/lab-08-01-code.zip
-unzip lab-08-01-code.zip
-useradd -m -s /bin/bash gcpadmin
-usermod -aG sudo gcpadmin
-echo "gcpadmin:lab-password" | chpasswd
-sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
-systemctl restart sshd.service
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get -y update
+sudo apt-get install -y php8.3
+sudo systemctl restart apache2.service
+cd /var/www/html/
+sudo rm index.html
+sudo wget https://github.com/ahmadzahoory/gcpcev2/raw/main/lab-08-01-code.zip
+sudo unzip lab-08-01-code.zip
+# Create user and set password
+sudo useradd -m -s /bin/bash gcpadmin
+sudo usermod -aG sudo gcpadmin
+sudo echo "gcpadmin:lab-password" | chpasswd
+# Turn on password authentication
+sudo sed -i 's|[#]*KbdInteractiveAuthentication no|#KbdInteractiveAuthentication no|g' /etc/ssh/sshd_config
+sudo sed -i 's|[#]*#PasswordAuthentication yes|PasswordAuthentication yes|g' /etc/ssh/sshd_config
+sudo systemctl restart ssh
